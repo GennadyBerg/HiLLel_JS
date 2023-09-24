9973f20edb9fc367f61e70b880da53ec312825c6
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2023-09-25 01:14:01 +0300

    hw2_complete

---
+++ lesson2/lesson2_hw/script.js
@@ -0,0 +1,7 @@
+let a = +prompt('Please, enter first number', '');
+let b = +prompt('Please, enter second number', '');
+alert(`Користувач ввів  ${a} i ${b}:
+ ${a} + ${b} = ${a+b};
+ ${a} - ${b} = ${a-b};
+ ${a} * ${b} = ${a*b};
+ ${a} / ${b} = ${a/b}`);
---
+++ lesson2/script.js
@@ -0,0 +1,31 @@
+//concat
+// first = 'random string';
+// second = 'second random string';
+// console.log(first);
+// console.log(second);
+// third = first + ' ' + ' any string ' + second;
+// console.log(third);
+
+
+///string literal
+
+// bigString = `Any new 
+// string`;
+// console.log(bigString);
+
+// count = 15;
+// bigStringInterpolate = `${count} apples`;
+// console.log(bigStringInterpolate);
+// userName = prompt('Say You name');
+// alert(`My name is ${userName}!!! Hello!`);
+
+
+const variable = `some text`;
+console.log(variable);
+let variable2 = 321;
+console.log(variable2);
+variable3 = 'any another';
+console.log(variable);
+const var2 = 124;
+
+

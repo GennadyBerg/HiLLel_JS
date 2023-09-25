cca1b8fa99b2176a2cbe3d7eed336820be3f1050
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2023-09-25 22:25:40 +0300

    hw  les3

---
+++ lesson3/lesson3_hw/script.js
@@ -0,0 +1,40 @@
+// 1-st relise             /////////////////////////////
+
+
+
+let fingerNum = +prompt('Enter any finger number? please', '');
+const fingerArr = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky_or_little'];
+if (fingerArr < 1 || fingerNum > 5 || fingerNum === 0 || !fingerNum) {
+            alert(`Please, enter correct value, or our respect to You, m-r(miss) Arrival! `);
+}
+else {
+            alert(`
+      You mean a ${fingerArr[fingerNum - 1]} finger
+      `);
+}
+console.log('End of code\)\)\)');
+
+
+// 2-nd relise ////////////////////////////
+
+let fingerNum1 = +prompt('Enter any finger number? please', '');
+if (fingerNum1 < 1 || fingerNum1 > 5 || fingerNum1 === 0 || !fingerNum1) {
+            alert(`Please, enter correct value, or our respect to You, m-r(miss) Arrival! `);
+}
+else if (fingerNum1 === 1) {
+            alert(`You mean a THUMB finger `);
+}
+
+else if (fingerNum1 === 2) {
+            alert(`You mean a INDEX finger `);
+}
+else if (fingerNum1 === 3) {
+            alert(`You mean a MIDDLE finger `);
+}
+else if (fingerNum1 === 4) {
+            alert(`You mean a RING finger `);
+}
+else if (fingerNum1 === 5) {
+            alert(`You mean a PINKY or LITTLE finger `);
+}
+console.log('End of code\)\)\)');
---
+++ lesson3/script.js
@@ -0,0 +1,30 @@
+// /// Homework
+//  let firstNumber = prompt ('Enret first number');
+// let secondNumber = prompt ('Enter second number');
+// firstNumber = +firstNumber;
+// secondNumber = +secondNumber;
+
+
+// 
+
+// const firstNumber = prompt('enter num',0 );
+// const secondNumber = prompt('enter num', 0 );
+// if (firstNumber > secondNumber) {
+//             alert('first number is greater')
+// }
+// // 
+// console.log('End of calc');
+
+// let currentTime = new Date().getHours();
+// console.log(currentTime);
+// if (currentTime < 10) {
+//             console.log('good morning');
+// }
+// else if (currentTime < 19) {
+//             console.log('good afternoon');
+// }
+// else {
+// console.log('good evening');
+
+// console.log('nice Time, friend!');
+

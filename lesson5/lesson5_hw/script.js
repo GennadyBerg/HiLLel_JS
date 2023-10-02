0a34991bf530f0510097c71d74983752ebe81358
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2023-10-02 22:04:13 +0300

    lesson5_hw_done

--- lesson5/lesson5_hw/script.js
+++ lesson5/lesson5_hw/script.js
@@ -1,3 +1,21 @@
 // Home Work
 
 
+let result = 1;
+const n = +prompt('Enter any natural number, please');
+while (n <= 0 || n % 1 !== 0) {
+            alert('You enter wrong number')
+            break;
+}
+if (n === 1) {
+            result = n;
+}
+else {
+            for (let i = 1; i < n; i++) {
+                        result += result * i;
+                        console.log(result);
+
+            }
+}
+console.log('Factorial', n, ' = ', result);
+alert(`Factorial ${n}  =  ${result}`);

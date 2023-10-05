6f8c5a9d1be06f0285f05ba1ddf3e8775dd69254
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2023-10-06 00:55:53 +0300

    second resolve

--- lesson6/lesson6_hw/script.js
+++ lesson6/lesson6_hw/script.js
@@ -1,3 +1,14 @@
 const arr = [false, 12, '23424', false, 324, null, 5, 0, 0, 9, undefined, NaN, 6, 7];
 const result = arr.filter(x=>x);
 console.log(result);
+
+//iterator
+// let i = 0;
+// const result = [];
+// arr.forEach(element => {
+//             if (element) {
+//                         result[i] = element;
+//                         i++;
+//             }
+//             console.log(result);
+// });

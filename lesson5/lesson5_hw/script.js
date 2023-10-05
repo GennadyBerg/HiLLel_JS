97e0fa4fbdbaae6881bac34b3bf21036d5aed493
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2023-10-05 19:42:28 +0300

    lesson6_begin

--- lesson5/lesson5_hw/script.js
+++ lesson5/lesson5_hw/script.js
@@ -2,10 +2,14 @@
 
 
 let result = 1;
-const n = +prompt('Enter any natural number, please');
-while (n <= 0 || n % 1 !== 0) {
-            alert('You enter wrong number')
+const n = +prompt('Enter any natural number, please',1);
+while (n < 0 || n % 1 !== 0 || n>50 ) {
+            alert('You enter wrong \(or too big\) number');
             break;
+            if (n===0 ) {
+                       console.log(`n! = ${result}`);
+                        
+            }
 }
 if (n === 1) {
             result = n;
@@ -13,9 +17,16 @@ if (n === 1) {
 else {
             for (let i = 1; i < n; i++) {
                         result += result * i;
-                        console.log(result);
+                        // console.log(result);
 
             }
 }
 console.log('Factorial', n, ' = ', result);
 alert(`Factorial ${n}  =  ${result}`);
+/// shot variant
+// let counter = 1;
+// while(n){
+//             counter*=n;
+//             n--;
+// }
+// console.log(counter);

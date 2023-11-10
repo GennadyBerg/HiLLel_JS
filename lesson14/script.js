36ec734f4b604005005ef6d791e13e846970c3b9
Gennady <111905354+GennadyBerg@users.noreply.github.com>
2023-11-10 09:18:01 +0200

    lesson14

---
+++ lesson14/script.js
@@ -0,0 +1,32 @@
+// const newDiv = document.createElement('div');
+// newDiv.innerHTML = 'Some new content';
+// document.body.prepend(newDiv);
+
+// console.log(newDiv);
+
+// const paragraph = document.createElement('p');
+// paragraph.innerHTML = 'new elem created';
+// document.body.append(paragraph);
+
+// console.log(paragraph);
+
+// const newLink = document.createElement('a');
+// newLink.innerHTML = 'New link';
+// newLink.id = 'myNewLink';
+// newLink.href = 'google.com';
+// document.body.append(newLink);
+
+// console.log(newLink);
+
+
+// const newDiwEl = document.createElement('div');
+// newDiwEl.id = 'uniqueId';
+// newDiwEl.innerHTML = 'newElem'
+// console.log(newDiwEl);
+// document.body.append(newDiwEl);
+
+// const uniqueId = document.getElementById('unique');
+// uniqueId.style.color = "red";
+// uniqueId.style.backgroundColor = yellow;
+
+const prew = document.createElement()
